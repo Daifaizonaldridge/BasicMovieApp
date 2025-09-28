22360b73e4bde7f4bedb60f6c2c522175e2bf868
Daifaizon J Aldridge <daifaizonaldridge@gmail.com>
2025-09-28 02:10:57 -0500

    Add project files.

---
+++ BasicMovieApp/Models/MovieModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BasicMovieApp.Models
+{
+    public class MovieModel
+    {
+        public int Id {get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
