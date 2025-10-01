8e117f3b34cc52e989732fd935bd588bc4a97ef6
Daifaizon J Aldridge <daifaizonaldridge@gmail.com>
2025-09-30 21:21:14 -0500

    focused on redunduncy and built pages important for movie review individula pages.

--- BasicMovieApp/Services/IMovieReviewService.cs
+++ BasicMovieApp/Services/IMovieReviewService.cs
@@ -5,6 +5,6 @@ namespace BasicMovieApp.Services
     public interface IMovieReviewService
     {
         List<MovieModel> GetMovies ();
-        MovieModel GetMovieById(int id);
+        MovieModel? GetMovieById(int id);
     }
 }
